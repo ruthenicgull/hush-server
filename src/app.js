7cf96fed0196feaa4d35cadcaefef3ab5c7c6db6
ruthenicgull <muhammedhisham2003@gmail.com>
2024-09-12 19:01:57 +0530

    updated cors to get origin fron env

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
@@ -5,8 +7,12 @@ import ErrorHandler from "./utils/ErrorHandler.js";
 
 const app = express();
 
-app.use(cors());
-
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN, // Frontend origin
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  })
+);
 // common middleware
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
